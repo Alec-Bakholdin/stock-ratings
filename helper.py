98ddb79c0b7b94c798009f42a77d5f4ec948d60c
Alec-Bakholdin-AIMS <alec@aims360.com>
2021-08-19 23:25:18 -0400

    chore: intialized project

---
+++ helper.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class LetterRating(Enum):
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    D = 'D'
+    F = 'F'
+    NA = None
---
+++ main.py
@@ -0,0 +1,5 @@
+from zacks import get_data as get_zacks_data
+
+
+zacks_data = get_zacks_data()
+print(zacks_data)
